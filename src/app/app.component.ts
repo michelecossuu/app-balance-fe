f30a736aa69b4804a28230cc569236f2acdf24a0
mcossu <michelecossu01@gmail.com>
2023-03-16 11:38:33 +0100

    aggiunto componente home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './home/home.component.html',
+  styleUrls: ['./home/home.component.css']
 })
 export class AppComponent {
   title = 'balance-app-fe';
