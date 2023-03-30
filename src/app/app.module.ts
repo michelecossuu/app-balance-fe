b8a04ac143e2519768f2d77ba8250d1a77dfbfb1
mcossu <michelecossu01@gmail.com>
2023-03-30 15:05:42 +0200

    home page work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
