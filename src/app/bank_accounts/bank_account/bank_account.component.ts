6d82d4ec1f6d296f1ad3463323611a4eea07ed77
mcossu <michelecossu01@gmail.com>
2023-04-05 11:44:47 +0200

    aggiunti file per la pagina con la lista dei  bank accounts e per i singoli bank account

---
+++ src/app/bank_accounts/bank_account/bank_account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-account',
+  templateUrl: './bank_account.component.html',
+  styleUrls: ['./bank_account.component.scss']
+})
+export class BankAccountComponent {
+  
+}
---
+++ src/app/bank_accounts/bank_accounts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-accounts',
+  templateUrl: './bank_accounts.component.html',
+  styleUrls: ['./bank_accounts.component.scss']
+})
+export class BankAccountsComponent {
+  
+}
