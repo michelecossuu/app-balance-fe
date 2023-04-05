8b4796a0e1d497780445f8aa81138df81324455f
mcossu <michelecossu01@gmail.com>
2023-04-05 11:43:35 +0200

    layout home completato. Da aggiungere: link per le altre pagine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HomeComponent } from './home/home.component';
+import { BankAccountsComponent } from './bank_accounts/bank_accounts.component';
+import { BankAccountComponent } from './bank_accounts/bank_account/bank_account.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    BankAccountsComponent,
+    BankAccountComponent
   ],
   imports: [
     BrowserModule
