f5675d51a7b44799b878f9951290c250fb655b7d
mcossu <michelecossu01@gmail.com>
2023-06-29 15:57:40 +0200

    aggiunta parte visiva della schermata BankAccounts

--- src/app/bank_accounts/bank_accounts.component.ts
+++ src/app/bank_accounts/bank_accounts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as labels from '../../assets/labels/labels.json';
 
 @Component({
   selector: 'app-bank-accounts',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./bank_accounts.component.scss']
 })
 export class BankAccountsComponent {
+  labels = labels;
+  totalBalance = 0;
+
+  bankAccounts: { name: string, balance: number }[] = [
+    { name: 'Conto 1', balance: 1000 },
+    { name: 'Conto 2', balance: 2000 },
+    { name: 'Conto 3', balance: 3000 },
+    { name: 'Conto 4', balance: 4000 }
+  ];
   
+  /**
+   * Metodo per calcolare il Total Balance
+   */
+  getTotalBalance(): void {
+    this.bankAccounts.forEach(element => {
+      this.totalBalance += element.balance;
+    });
+  }
+  
+  /**
+   * Calcolo il Total Balance una volta sola
+   */
+  ngOnInit(): void {
+    this.getTotalBalance();
+  }
+
 }
